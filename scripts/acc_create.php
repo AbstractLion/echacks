24e4489349e4f18ed630d79fe68a2658b089b6f9
AbstractUltra <dragonhe36@gmail.com>
2019-11-02 14:06:29 -0400

    Done some stuff

--- scripts/acc_create.php
+++ scripts/acc_create.php
@@ -7,5 +7,14 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     } else {
         $password_hash = password_hash($_POST['password'], PASSWORD_BCRYPT);
         $db->query("insert into accounts (username, password, email, display_name, uuid, api_key) values ('{$_POST['username']}','{$password_hash}','{$_POST['email']}','{$_POST['display_name']}',UUID(),'{$_POST['api_key']}')");
+
+        $uuid = null;
+        $get_uuid = $db->query("select uuid from accounts where username = '{$_POST['username']}'");
+        extract($get_uuid->fetch_assoc());
+
+        $expiry_time = time() + 3000000;
+        $session_uuid = uniqid();
+        $db->query("insert into active_sessions (session_uuid, user_uuid, expiry) values ('{$session_uuid}', '{$uuid}', '{$expiry_time}')");
+        setcookie("SESSION_UUID", $session_uuid, $expiry_time, "/");
     }
 }
