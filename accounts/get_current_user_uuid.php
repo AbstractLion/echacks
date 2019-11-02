40b66085f43bb92382267017e109db9d3d23078f
AbstractUltra <dragonhe36@gmail.com>
2019-11-02 10:44:44 -0400

    Added function to get user uuid

--- accounts/get_current_user_uuid.php
+++ accounts/get_current_user_uuid.php
@@ -1,4 +1,5 @@
 <?php
+include "../database_connect.php";
 $user_uuid = null;
 $get_user_uuid = $sql_connection->query("select user_uuid from active_sessions where session_uuid = '{$_COOKIE['SESSION_UUID']}'");
 if ($get_user_uuid->num_rows >= 1) {
