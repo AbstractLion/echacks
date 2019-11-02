2c94a861fcc409ebcca1e0d7c0858c7b8db42e94
AbstractUltra <dragonhe36@gmail.com>
2019-11-01 23:55:15 -0400

    Added API Key to database

--- accounts/acc_create.php
+++ accounts/acc_create.php
@@ -2,5 +2,5 @@
 include "../database_connect.php";
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $password_hash = password_hash($_POST['password'],PASSWORD_BCRYPT);
-    $sql_connection->query("insert into accounts (username, password, email, display_name, uuid) values ('{$_POST['username']}','{$password_hash}','{$_POST['email']}','{$_POST['display_name']}',UUID())");
+    $sql_connection->query("insert into accounts (username, password, email, display_name, uuid, api_key) values ('{$_POST['username']}','{$password_hash}','{$_POST['email']}','{$_POST['display_name']}',UUID(),'{$_POST['api_key']}')");
 }
--- database_connect.php
+++ database_connect.php
@@ -3,4 +3,4 @@ include "credentials/sql_credentials.php";
 $sql_connection = new mysqli($server_name,$username,$password,$database_name);
 
 // Create necessary tables
-$sql_connection->query("create table if not exists accounts (id integer primary key auto_increment, username text, password text, email text, display_name text, uuid text)");
+$sql_connection->query("create table if not exists accounts (id integer primary key auto_increment, username text, password text, email text, display_name text, uuid text, api_key text)");
